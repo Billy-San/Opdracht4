e001b2f45aa1ed32e19dc1aec64c8ab93cd380be
Billy-San <bilal.bakkali@student.ehb.be>
2022-10-23 23:14:13 +0200

    Add project files.

---
+++ FouteRij.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    internal class FouteRij<T>
+    {
+        private List<T> rij = new List<T>();
+        private List<T> rij2 = new List<T>();
+        private T Value;
+
+
+        public T inDeRij(T element)
+        {
+            rij.Add(element);
+            if (rij.Count == 1)
+            {
+                Value = element;
+            }
+            return Value;
+        }
+
+        public T Delete()
+        {
+            rij.Remove(Value);
+            if (rij.Count > 0)
+            {
+                Value = rij[0];
+            }
+            return Value;
+        }
+
+        public T Next()
+        {
+            if (rij.Count > 0)
+            {
+                int rowNumber = rij.IndexOf(Value);
+                if (rowNumber == rij.Count - 1)
+                {
+                    return rij[0];
+                }
+                else
+                {
+                    return rij[rowNumber + 1];
+                }
+            }
+            return Value;
+        }
+
+        public bool isEmpty()
+        {
+            if (rij.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public int Count()
+        {
+            return rij.Count;
+        }
+
+        public T ZetAchteraan()
+        {
+            if (rij.Count <= 0)
+            {
+                rij.Remove(Value);
+                rij.Add(Value);
+                Value = rij[0];
+            }
+            return Value;
+
+        }
+
+        public void LeegMaken()
+        {
+            rij.Clear();
+        }
+
+        public override string ToString()
+        {
+            string showList = "";
+
+            foreach (T ele in rij)
+            {
+                showList += ele.ToString() + "\n" + " " + ", ";
+            }
+
+            return showList;
+        }
+
+        public List<T> rijCopy()
+        {
+            rij2 = rij.GetRange(0, rij.Count);
+
+            return rij2;
+        }
+    }
+}
---
+++ TeDoen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    internal class TeDoen
+    {
+            private int Id { get; set; }
+            private static int count = 0;
+            private DateTime? TijdStip { get; set; }
+            private string Title { get; set; }
+            private string[] Info;
+            private string text;
+            private string[] lines;
+
+            public TeDoen (DateTime tijdStip, string title, string[] informatie)
+            {
+                if (tijdStip >= DateTime.Now)
+                {
+                    TijdStip = tijdStip;
+                }
+                else
+                {
+                    TijdStip = null;
+                }
+
+                Title = title;
+                Info = informatie;
+                count++;
+                Id = count;
+            }
+
+            public override string ToString()
+            {
+                string result = "";
+                if (TijdStip != null)
+                {
+                    return "id " + Id + " title: " + Title + " information: " + result;
+                }
+                else
+                {
+                    return "id " + Id + " tijdstijd: " + TijdStip + " title: " + Title + " information: " + result;
+                }
+            }
+
+
+        }
+}
